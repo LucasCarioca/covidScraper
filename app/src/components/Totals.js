bdd2d586f0f536d0c091260c3dc7c97440f99a48
Lucas Desouza <lucasfds2777@gmail.com>
2020-12-11 09:53:05 -0600

    add details no what icu rate means

--- app/src/components/Totals.js
+++ app/src/components/Totals.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {TableContainer, TableRow} from "@material-ui/core";
+import {TableContainer, TableRow, Typography} from "@material-ui/core";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -22,49 +22,52 @@ const Totals = () => {
 
     const content = () => {
         return (
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Index</TableCell>
-                            <TableCell>Count</TableCell>
-                            <TableCell>Rate</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell><strong>Total cases</strong></TableCell>
-                            <TableCell>{numberWithCommas(data.total)}</TableCell>
-                            <TableCell></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell><strong>Total deaths</strong></TableCell>
-                            <TableCell>{numberWithCommas(data.deaths)}</TableCell>
-                            <TableCell>{data.deathRate.toFixed(2)}%</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell><strong>Total recovered</strong></TableCell>
-                            <TableCell>{numberWithCommas(data.recovered)}</TableCell>
-                            <TableCell>{data.recoveryRate.toFixed(2)}%</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell><strong>Currently infected</strong></TableCell>
-                            <TableCell>{numberWithCommas(data.currentlyInfected)}</TableCell>
-                            <TableCell></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell><strong>Currently hospitalized</strong></TableCell>
-                            <TableCell>{numberWithCommas(data.currentlyHospitalized)}</TableCell>
-                            <TableCell>{data.hospitalizedRate.toFixed(2)}%</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell><strong>Currently in ICU</strong></TableCell>
-                            <TableCell>{numberWithCommas(data.currentlyInICU)}</TableCell>
-                            <TableCell>{data.inICURate.toFixed(2)}%</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Index</TableCell>
+                                <TableCell>Count</TableCell>
+                                <TableCell>Rate</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell><strong>Total cases</strong></TableCell>
+                                <TableCell>{numberWithCommas(data.total)}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell><strong>Total deaths</strong></TableCell>
+                                <TableCell>{numberWithCommas(data.deaths)}</TableCell>
+                                <TableCell>{data.deathRate.toFixed(2)}%</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell><strong>Total recovered</strong></TableCell>
+                                <TableCell>{numberWithCommas(data.recovered)}</TableCell>
+                                <TableCell>{data.recoveryRate.toFixed(2)}%</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell><strong>Currently infected</strong></TableCell>
+                                <TableCell>{numberWithCommas(data.currentlyInfected)}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell><strong>Currently hospitalized</strong></TableCell>
+                                <TableCell>{numberWithCommas(data.currentlyHospitalized)}</TableCell>
+                                <TableCell>{data.hospitalizedRate.toFixed(2)}%</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell><strong>Currently in ICU</strong></TableCell>
+                                <TableCell>{numberWithCommas(data.currentlyInICU)}</TableCell>
+                                <TableCell>*{data.inICURate.toFixed(2)}%</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <Typography>* Percent of hospitalized patients that are currently in the ICU.</Typography>
+            </>
         )
     }
 
